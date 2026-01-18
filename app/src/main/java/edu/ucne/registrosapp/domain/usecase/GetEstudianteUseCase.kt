e6f281fcad16d48eb28972b72f60b643c044056f
stevenmjl <stevenmjl.24@gmail.com>
2026-01-18 19:28:13 -0400

    Agregando el GetEstudianteUseCase Para buscar uno solo por ID.

---
+++ app/src/main/java/edu/ucne/registrosapp/domain/usecase/GetEstudianteUseCase.kt
@@ -0,0 +1,13 @@
+package edu.ucne.registrosapp.domain.usecase
+
+import edu.ucne.registrosapp.domain.models.Estudiante
+import edu.ucne.registrosapp.domain.repository.EstudianteRepository
+import javax.inject.Inject
+
+class GetEstudianteUseCase @Inject constructor(
+    private val repository: EstudianteRepository
+) {
+    suspend operator fun invoke(id: Int): Estudiante? {
+        return repository.getEstudiante(id)
+    }
+}
