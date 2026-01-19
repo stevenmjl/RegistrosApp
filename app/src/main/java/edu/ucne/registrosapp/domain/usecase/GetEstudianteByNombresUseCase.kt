2077dc341fc95454d27f184ca202188651e275f5
stevenmjl <stevenmjl.24@gmail.com>
2026-01-19 15:15:06 -0400

    Agregando GetEstudianteByNombresUseCase.

---
+++ app/src/main/java/edu/ucne/registrosapp/domain/usecase/GetEstudianteByNombresUseCase.kt
@@ -0,0 +1,13 @@
+package edu.ucne.registrosapp.domain.usecase
+
+import edu.ucne.registrosapp.domain.models.Estudiante
+import edu.ucne.registrosapp.domain.repository.EstudianteRepository
+import javax.inject.Inject
+
+class GetEstudianteByNombresUseCase @Inject constructor(
+    private val repository: EstudianteRepository
+) {
+    suspend operator fun invoke(nombres: String): Estudiante? {
+        return repository.getByNombres(nombres)
+    }
+}
