396853eac49296184b7b6a74a11ed431e6e66867
stevenmjl <stevenmjl.24@gmail.com>
2026-01-19 14:56:42 -0400

    Cambiando el tema a unos colores azules.

--- app/src/main/java/edu/ucne/registrosapp/ui/theme/Color.kt
+++ app/src/main/java/edu/ucne/registrosapp/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package edu.ucne.registrosapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Azules para el Tema Claro (más intensos)
+val BlueLightPrimary = Color(0xFF0056D2)
+val BlueLightSecondary = Color(0xFF535F70)
+val BlueLightTertiary = Color(0xFF6B5778)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Azules para el Tema Oscuro (más suaves/pasteles)
+val BlueDarkPrimary = Color(0xFFB0C6FF)
+val BlueDarkSecondary = Color(0xFFBBC7DB)
+val BlueDarkTertiary = Color(0xFFD6BEE4)
