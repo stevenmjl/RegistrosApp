6b3272daeb8a55dc2eed99dc731a710999c7c5cf
stevenmjl <stevenmjl.24@gmail.com>
2026-01-24 02:23:39 -0400

    Modificando DrawerMenu.kt con Asignatura

--- app/src/main/java/edu/ucne/registrosapp/presentation/navigation/DrawerMenu.kt
+++ app/src/main/java/edu/ucne/registrosapp/presentation/navigation/DrawerMenu.kt
@@ -1,22 +1,13 @@
 package edu.ucne.registrosapp.presentation.navigation
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.MenuBook
+import androidx.compose.material.icons.filled.MenuBook // Nuevo ícono
 import androidx.compose.material.icons.filled.People
-import androidx.compose.material3.DrawerState
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -58,6 +49,7 @@ fun DrawerMenu(
                 Spacer(modifier = Modifier.height(16.dp))
 
                 LazyColumn {
+                    // Item de Estudiantes
                     item {
                         DrawerItem(
                             title = stringResource(R.string.drawer_estudiantes),
@@ -65,6 +57,17 @@ fun DrawerMenu(
                             isSelected = selectedItem.value == stringResource(R.string.drawer_estudiantes)
                         ) {
                             handleItemClick(Screen.EstudianteList, it)
+                        }
+                    }
+
+                    // Item de Asignaturas
+                    item {
+                        DrawerItem(
+                            title = stringResource(R.string.drawer_asignaturas),
+                            icon = Icons.AutoMirrored.Filled.MenuBook,
+                            isSelected = selectedItem.value == stringResource(R.string.drawer_asignaturas)
+                        ) {
+                            handleItemClick(Screen.AsignaturaList, it)
                         }
                     }
                 }
