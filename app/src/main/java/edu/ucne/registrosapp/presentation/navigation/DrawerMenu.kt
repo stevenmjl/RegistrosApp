14c1e71568fdc86bf96ebb646a64f9fb231604aa
stevenmjl <stevenmjl.24@gmail.com>
2026-01-24 04:33:50 -0400

    Modificando DrawerMenu.kt con nuevo preview

--- app/src/main/java/edu/ucne/registrosapp/presentation/navigation/DrawerMenu.kt
+++ app/src/main/java/edu/ucne/registrosapp/presentation/navigation/DrawerMenu.kt
@@ -1,20 +1,35 @@
 package edu.ucne.registrosapp.presentation.navigation
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.MenuBook
-import androidx.compose.material.icons.filled.MenuBook // Nuevo ícono
 import androidx.compose.material.icons.filled.People
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import edu.ucne.registrosapp.R
+import edu.ucne.registrosapp.ui.theme.RegistrosAppTheme
 import kotlinx.coroutines.launch
 
 @Composable
@@ -76,5 +91,17 @@ fun DrawerMenu(
         drawerState = drawerState
     ) {
         content()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DrawerMenuPreview() {
+    RegistrosAppTheme {
+        DrawerMenu(
+            drawerState = rememberDrawerState(initialValue = DrawerValue.Open),
+            navHostController = rememberNavController(),
+            content = {}
+        )
     }
 }
